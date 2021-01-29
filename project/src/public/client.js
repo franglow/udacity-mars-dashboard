ac23c5a7bd98616fb70618ca5144a5b49870d433
Fran <elfrancortez@gmail.com>
2021-01-29 14:31:55 +0100

    first bulky commit : )

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -2,12 +2,14 @@ let store = {
     user: { name: "Student" },
     apod: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    rover_album_thumbs: [],
 }
 
 // add our markup to the page
 const root = document.getElementById('root')
 
 const updateStore = (store, newState) => {
+    console.log('updating store:',newState);
     store = Object.assign(store, newState)
     render(root, store)
 }
@@ -18,7 +20,10 @@ const render = async (root, state) => {
 
 
 // create content
-const App = (state) => {
+const Appes = (state) => {
+//FIXME
+// This function used to be called App, 
+// const App = (state) => {
     let { rovers, apod } = state
 
     return `
@@ -43,6 +48,41 @@ const App = (state) => {
     `
 }
 
+// My App function
+const App = (state) => {
+    let { rovers, rover_album_thumbs } = state
+
+    //FIXME
+    console.log('state', state);
+    return `
+        <header></header>
+        <main>
+            ${Greeting(store.user.name)}
+            <section>
+                <h3>Put things on the page!</h3>
+                <p>Here is an example section.</p>
+            <div id = "grid-container">
+		
+                <div class="flex-grid-3">
+                    ${RoverIconList(rovers)}
+                </div>
+                
+                
+                <div class="flex-grid-3">
+                    ${RoverGalleryAlbumThumbs(rover_album_thumbs)}
+                </div>
+                
+                
+            </div>
+          
+                
+            </section>
+        </main>
+        <footer></footer>
+    `
+}
+
+
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
     render(root, store)
@@ -65,6 +105,8 @@ const Greeting = (name) => {
 
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (apod) => {
+    console.log('en apod:',apod);
+console.log('en store:',store);
 
     // If image does not already exist, or it is not from today -- request it again
     const today = new Date()
@@ -75,7 +117,6 @@ const ImageOfTheDay = (apod) => {
     if (!apod || apod.date === today.getDate() ) {
         getImageOfTheDay(store)
     }
-
     // check if the photo of the day is actually type video!
     if (apod.media_type === "video") {
         return (`
@@ -91,15 +132,128 @@ const ImageOfTheDay = (apod) => {
     }
 }
 
+// My Pure function
+const RoverPhoto = (photo) => {
+console.log('en photo:',photo);
+console.log('en store:',store);
+    // to avoid fetch loop!
+    if (!photo)  getRoverLatestPhotos(store)
+}
+
+const RoverIconList = (rovers) => {
+    //Deberia llamar ala function que muestra
+    // los en una lista horizontal en iconitos
+    // los rovers disponibles.
+    
+    // rovers.forEach(item => 
+    //     main-rover-icon.style.setProperty('--main-rover-icon',"./assets/images/icons/" + item.toLowerCase() + ".svg")
+    // );
+
+    // looping template strings
+    // https://wesbos.com/template-strings-html
+/**
+ * esto iba en el return
+ * <li>
+                    <div class="main-rover-icon">
+                        <img src="./assets/images/icons/${item.toLowerCase()}.svg" />
+                    <div>${item}</div>
+                </li>
+*/
+    return `
+        ${rovers.map(item => {
+            return `
+                <div class="grid-col">
+                    <img class = "container-thumb" src="./assets/images/icons/${item.toLowerCase()}.svg" ></img>
+                    <p class = "thumb-title">${item}</p>
+                </div>
+            `
+        }).join('')}
+    `
+}
+
+const RoverGalleryAlbumThumbs = (rover_album_thumbs) => {
+    //LLammada ala fncion que lista horizontal//
+    // los botones para ir a ver las galerias
+    // de las latest fotos de los rovers
+    // Los botones seran miniaturas de la primera
+    // foto de cada latest.
+    // rovers.forEach(item => {
+    // });
+    RoverAlbumThumbs(rover_album_thumbs)
+    return `
+        ${rover_album_thumbs.map(item => {
+            return `
+                <div class="grid-col">
+                    <img class = "container-thumb" src="${item.photo_url}" ></img>
+                </div>
+            `
+            }).join('')}
+    `
+
+}
+
+const RoverAlbumThumbs = (rover_album_thumbs) => {
+    console.log('en store:',store);
+    console.log('en rover_album_thumbs:',rover_album_thumbs);
+    if (!rover_album_thumbs.length) getRoverAlbumThumbs(store)
+} 
+
 // ------------------------------------------------------  API CALLS
 
 // Example API call
 const getImageOfTheDay = (state) => {
     let { apod } = state
-
     fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        // apod is that image object created as res.send({image})
+        .then(apod => updateStore(store, { apod })) 
 
-    return data
+    // return data
+}
+
+// My API calls
+const getRoverLatestPhotos = (state) => {
+    let { photos } = state
+    let rover = 'curiosity'
+    let camera = 'fhaz'
+    fetch(`http://localhost:3000/latest_photos?Rover=${rover}&Camera=${camera}`)
+        .then(res => {
+            console.log('res',res);
+            return res.json()})
+        .then(rover_info => {
+            console.log(state);
+            // check rover info
+            console.log('rover_info: ',rover_info);
+
+            if (!Object.entries(rover_info).length) {
+                //FIXME
+                console.log('Here I could suggest available cameras');
+            }
+
+            updateStore(store, { rover_info })
+        })
+}
+
+const getRoverAlbumThumbs = (state) => {
+    const { rover_album_thumbs, rovers } = state
+    //este metodo debe solicitar los 3 thumbs
+    let res_arr = []
+    rovers.forEach((item,i) => {
+        fetch(`http://localhost:3000/photos?Rover=${item}`)
+        .then(res => res.json())
+        .then(rover_photo => {
+            rover_album_thumbs.push(rover_photo)
+            // Workaround due to updateStore object assign()
+            // Make sure all rovers were reached
+            if ( rovers.length - 1 === i ) {
+                return updateStore(store, { rover_album_thumbs })
+            }
+        }) 
+    })
+
+}
+
+// Call for Rover photos for pictures page
+const getRoverPhotos = (state) => {
+
 }
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+const { Z_FIXED } = require('zlib')
+const { response } = require('express')
 
 const app = express()
 const port = 3000
@@ -13,6 +15,52 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
+app.get('/latest_photos', async (req, res) => {
+    try {
+        let response = await fetch(
+            // `https://api.nasa.gov/mars-photos/api/v1/rovers/${req.query.Rover}/latest_photos?camera=${req.query.Camera}&api_key=${process.env.API_KEY}`)
+            `https://api.nasa.gov/mars-photos/api/v1/rovers/${req.query.Rover}/latest_photos?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        if (!response.error && response.latest_photos.length) {
+            let rover_info = {
+                name : response.latest_photos[0].rover.name,
+                launch_date : response.latest_photos[0].rover.launch_date,
+                landing_date : response.latest_photos[0].rover.landing_date,
+                status : response.latest_photos[0].rover.status,
+                most_recent_photo_url : response.latest_photos[0].img_src,
+                photo_taken_date : response.latest_photos[0].earth_date,
+                camera : response.latest_photos[0].camera.name
+            }
+            //http://expressjs.com/en/5x/api.html#res.send
+            // res.send(rover_info)
+        } else {
+            res.send({})
+        }
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+app.get('/photos', async (req, res) => {
+    // deberia devolber los 3 thumbs
+    console.log('Rover: ',req.query.Rover);
+    try {
+        let response = await fetch(
+            `https://api.nasa.gov/mars-photos/api/v1/rovers/${req.query.Rover}/photos?sol=1&page=1&api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        if (!response.error && response.photos.length) {
+            let rover_photo = {
+                rover_name : response.photos[0].rover.name,
+                photo_url : response.photos[0].img_src
+            }
+            res.send(rover_photo)
+        } else {
+            res.send({})
+        }
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
 
 // example API call
 app.get('/apod', async (req, res) => {
